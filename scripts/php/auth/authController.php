8c2a47e4349df55e30631d3a1c243e072a105252
Betadinho <alpha-prechtl@outlook.com>
2019-06-24 03:44:44 +0200

    Fix Logout Issue
    came up after changing the logout button from an anchor to a link 
    
    FIX: redirect to index.php in logout function

--- scripts/php/auth/authController.php
+++ scripts/php/auth/authController.php
@@ -27,6 +27,10 @@ switch ($method) {
         if (isset($_GET['logout'])) {
           unset($_SESSION['username']);
           session_destroy();
+          $URL = '../../../index.php';
+          echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+          echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+          exit;
         }
         break;
     case 'POST':
